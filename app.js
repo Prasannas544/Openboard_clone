38cac871b971ff8b6d1b296241d74347a124e43e
Prasannas544 <prasannas544@gmail.com>
2022-06-21 23:59:09 +0530

    all done

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express"); //Access
+const socket = require("socket.io");
+
+const app = express(); //Initialsed and server ready 
+
+
+app.use(express.static("public")); // display my index.html file 
+
+let port =3000;
+let server= app.listen(port,() => {
+
+    console.log("Listening to port " + port);
+})
+
+let io = socket(server);
+
+io.on("connection",(socket)=>{
+    console.log("Made socket connection");
+    
+    //recieved data
+    socket.on("beginPath",(data)=>{
+        //trasnfer data to all workstations
+        io.sockets.emit("beginPath",data);
+    })
+
+    socket.on("drawStroke",(data)=>{
+        //trasnfer data to all workstations
+        io.sockets.emit("drawStroke",data);
+    })
+
+    socket.on("redoUndo",(data)=>{
+        //trasnfer data to all workstations
+        io.sockets.emit("redoUndo",data);
+    })
+})
